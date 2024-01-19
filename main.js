ae2cc5c723b04a18f7408ea478f85940f8182eb0
Max Chechel <maksymchechel@yahoo.com>
2024-01-19 09:13:08 +0100

    smooth scroll

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('hi');
+import Lenis from '@studio-freight/lenis';
+
+const lenis = new Lenis({
+    duration: 1.5,
+    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+    direction: 'vertical',
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 1.5,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
