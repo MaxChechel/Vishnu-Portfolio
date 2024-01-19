0b199c9c8fb24f06ee3c6b0bef3c62d5bda14a42
Max Chechel <maksymchechel@yahoo.com>
2024-01-19 09:28:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,26 +3,28 @@ import Lenis from '@studio-freight/lenis';
 const worksLink = document.querySelector('#works-link');
 const contactLink = document.querySelector('#contact-link');
 
-const lenis = new Lenis({
-    duration: 1.5,
-    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(1 - t, 4)),
-    direction: 'vertical',
-    smooth: true,
-    smoothTouch: false,
-    touchMultiplier: 1.5,
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const lenis = new Lenis({
+        duration: 1.5,
+        easing: (t) => (t === 1 ? 1 : 1 - Math.pow(1 - t, 4)),
+        direction: 'vertical',
+        smooth: true,
+        smoothTouch: false,
+        touchMultiplier: 1.5,
+    });
 
-function raf(time) {
-    lenis.raf(time);
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
     requestAnimationFrame(raf);
-}
 
-requestAnimationFrame(raf);
-
-//Scroll to for navigation links on click
-worksLink.addEventListener('click', () => {
-    lenis.scrollTo('#projects');
-});
-contactLink.addEventListener('click', () => {
-    lenis.scrollTo('#bottom');
+    //Scroll to for navigation links on click
+    worksLink.addEventListener('click', () => {
+        lenis.scrollTo('#projects');
+    });
+    contactLink.addEventListener('click', () => {
+        lenis.scrollTo('#bottom');
+    });
 });
