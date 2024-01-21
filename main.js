ed9c2243c5ed9d1adbb6056c55cea72823702e25
Max Chechel <maksymchechel@yahoo.com>
2024-01-21 17:29:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
             onEnter: () => {
                 gsap.to(section.querySelectorAll('.char'), {
                     opacity: 1,
-                    stagger: { each: 0.025 },
+                    stagger: { each: 0.005 },
                 });
             },
         });
